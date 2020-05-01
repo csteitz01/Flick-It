2d1e22746161e10fd7caf7b866d2d8fb7eb30dc9
csteitz01 <csteitz01@bellarmine.edu>
2020-04-30 20:30:50 -0400

    Initial commit

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+
+    [SerializeField] float waitToLoad = 4f;
+    [SerializeField] GameObject winLabel;
+    [SerializeField] GameObject loseLabel;
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+        loseLabel.SetActive(false);
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
+    }
+
+    public void HandleLoseCondition()
+    {
+        loseLabel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    
+}
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+
+    [SerializeField] Vector3 rotateSpeed;
+    Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        Quaternion deltaRotation = Quaternion.Euler(rotateSpeed * Time.deltaTime);
+        Quaternion oppositeRotation = Quaternion.Inverse(deltaRotation);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * oppositeRotation);
+        }
+    }
+}
